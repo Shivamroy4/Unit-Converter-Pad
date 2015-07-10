d7ac8b3ebade2f73af4ceaa273795f4d4a5feb98
Shivam Roy <shivamroyhell@gmail.com>
2015-07-11 00:08:45 +0530

    Added Kilometer conversion operations

--- Units Converter Pad/ViewController.swift
+++ Units Converter Pad/ViewController.swift
@@ -187,9 +187,13 @@ func DoubletoString(UnitOutput: Double) ->String{
    
         // Shows UITextField when corresponding button is pressed
         
-        @IBAction func MeterButton(sender: UIButton) {
         
-        Meter.hidden = false
+        @IBAction func MeterButtonPressed(sender: UIButton) {
+            
+            Meter.hidden = false
+            
+            Meter.userInteractionEnabled = true
+            
             
             // Hiding other UITextFields on Button press
             
@@ -220,13 +224,12 @@ func DoubletoString(UnitOutput: Double) ->String{
             FathomConvertButton.hidden = true
             DecimeterConvertButton.hidden = true
             
-            
-        
-   
-        
+
         }
+       
     
-       // Shows CONVERT Button when data is entered into the UITextField
+       // Shows CONVERT Button when data is entered into the UITextField .
+        // * Editing Did Change *.
         
         
         @IBAction func MeterInput(sender: AnyObject) {
@@ -347,7 +350,172 @@ func DoubletoString(UnitOutput: Double) ->String{
         HideKeyboard()
         }
    
+     
         
+        // Kilometer operations
+        
+        
+        @IBAction func KilometerButtonPressed(sender: UIButton) {
+            
+            
+            Kilometer.hidden = false
+            
+            Kilometer.userInteractionEnabled = true
+            
+            
+            // Hiding other UITextFields on Button press
+            
+            Meter.hidden = true
+            Inch.hidden = true
+            Foot.hidden = true
+            Mile.hidden = true
+            Angstrom.hidden = true
+            Centimeter.hidden = true
+            Yard.hidden = true
+            Furlong.hidden = true
+            Hand.hidden = true
+            Fathom.hidden = true
+            Decimeter.hidden = true
+            
+            
+            // Hiding other Convert Buttons on Button press
+            
+            MeterConvertButton.hidden = true
+            InchConvertButton.hidden = true
+            FootConvertButton.hidden = true
+            MileConvertButton.hidden = true
+            AngstromConvertButton.hidden = true
+            CentimeterConvertButton.hidden = true
+            YardConvertButton.hidden = true
+            FurlongConvertButton.hidden = true
+            HandConvertButton.hidden = true
+            FathomConvertButton.hidden = true
+            DecimeterConvertButton.hidden = true
+            
+
+        }
+        
+        // Shows Convert Button when data is entered .
+        
+        @IBAction func KilometerInput(sender: UITextField) {
+            if Kilometer.text.isEmpty == false {
+                
+                KilometerConvertButton.hidden = false
+                MeterConvertButton.hidden = true
+                
+            }
+
+        }
+        
+        // Kilometer to other unit conversions
+        
+        @IBAction func KilometerConvertButtonPressed(sender: UIButton) {
+            
+        var KilometerValue = StringtoDouble(Kilometer)
+            
+            
+            var MeterValue = KilometerValue * 1000
+            
+            var MeterDisplay = DoubletoString(MeterValue)
+            
+            DisplayUnit(Meter,UnitDisplay: MeterDisplay)
+            
+            
+        
+            
+            // Meter to Inch
+            
+            var InchOutput: Double = MeterValue * 39.37007874
+            
+            var InchDisplay: String = DoubletoString(InchOutput)
+            
+            DisplayUnit(Inch,UnitDisplay: InchDisplay)
+            
+            // Meter to Foot
+            
+            var FootOutput: Double = MeterValue * 3.2808399
+            
+            var FootDisplay: String = DoubletoString(FootOutput)
+            
+            DisplayUnit(Foot,UnitDisplay: FootDisplay)
+            
+            
+            // Meter to Mile
+            
+            var MileOutput: Double = MeterValue * 0.00062137
+            
+            var MileDisplay: String = DoubletoString(MileOutput)
+            
+            DisplayUnit(Mile,UnitDisplay: MileDisplay)
+            
+            
+            // Meter to Angtrom
+            
+            var AngstromOutput: Double = MeterValue * 10000000000
+            
+            var AngstromDisplay: String = DoubletoString(AngstromOutput)
+            
+            DisplayUnit(Angstrom,UnitDisplay: AngstromDisplay)
+            
+            // Meter to Centimeter
+            
+            var CentimeterOutput: Double = MeterValue * 100
+            
+            var CentimeterDisplay: String = DoubletoString(CentimeterOutput)
+            
+            DisplayUnit(Centimeter,UnitDisplay: CentimeterDisplay)
+            
+            
+            // Meter to Yard
+            
+            var YardOutput: Double = MeterValue * 1.0936133
+            
+            var YardDisplay: String = DoubletoString(YardOutput)
+            
+            DisplayUnit(Yard,UnitDisplay: YardDisplay)
+            
+            
+            // Meter to Furlong
+            
+            var FurlongOutput: Double = MeterValue * 0.00497097
+            
+            var FurlongDisplay: String = DoubletoString(FurlongOutput)
+            
+            DisplayUnit(Furlong,UnitDisplay: FurlongDisplay)
+            
+            
+            // Meter to Hand
+            
+            var HandOutput: Double = MeterValue * 9.84251969
+            
+            var HandDisplay: String = DoubletoString(HandOutput)
+            
+            DisplayUnit(Hand,UnitDisplay: HandDisplay)
+            
+            
+            // Meter to Fathom
+            
+            var FathomOutput: Double = MeterValue * 0.54680665
+            
+            var FathomDisplay: String = DoubletoString(FathomOutput)
+            
+            DisplayUnit(Fathom,UnitDisplay: FathomDisplay)
+            
+            
+            // Meter to Decimeter
+            
+            var DecimeterOutput: Double = MeterValue * 10
+            
+            var DecimeterDisplay: String = DoubletoString(DecimeterOutput)
+            
+            DisplayUnit(Decimeter,UnitDisplay: DecimeterDisplay)
+            
+            
+            
+            HideKeyboard()
+            
+            
+        }
         
    }
 
